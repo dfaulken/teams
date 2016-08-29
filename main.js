b24903491c08d4763ad04590f49fbd424bbd4101
David Faulkenberry <david.faulkenberry@gmail.com>
2016-08-29 14:25:48 -0400

    Track who's in what team

--- main.js
+++ main.js
@@ -1,5 +1,20 @@
 $(document).ready(function(){
-  $('.programmer').draggable({
-  }).droppable({
-  });
+  function toggleLead(){
+    $(this).toggleClass('lead');
+  }
+
+  function programmerDropped(event, ui){
+    var teamName = $(this).parent().data('name');
+    var programmer = ui.draggable;
+    programmer.data('team-name', teamName);
+  }
+
+  function setProgrammerTeam(){
+    var teamName = $(this).parents('.team').data('name');
+    $(this).data('team-name', teamName);
+  }
+
+  $('.programmer').draggable().each(setProgrammerTeam);
+  $('.programmer:not(.in-training)').dblclick(toggleLead);
+  $('.team .container').droppable({ drop: programmerDropped });
 });
