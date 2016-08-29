ee335a65bf0cc8cf8c1e15a51ed0c7cd8025ae67
David Faulkenberry <david.faulkenberry@gmail.com>
2016-08-29 15:04:45 -0400

    Add underscore; count number of each kind of programmer in team

--- main.js
+++ main.js
@@ -1,12 +1,32 @@
 $(document).ready(function(){
-  function toggleLead(){
-    $(this).toggleClass('lead');
+
+  function checkTeams(){
+    $('.team').each(function(){
+      var teamName = $(this).data('name');
+      var programmers = $('.programmer').filter(function(){
+        return $(this).data('teamName') == teamName;
+      }).map(function(){ 
+        return $(this).attr('class').split(' ');
+      });
+      $(this).toggleClass('lead-missing', count(programmers, 'lead') < 1);
+      $(this).toggleClass('lead-surplus', count(programmers, 'lead') > 1);
+      $(this).toggleClass('trainee-missing', count(programmers, 'trainee') < 1);
+      $(this).toggleClass('trainee-surplus', count(programmers, 'trainee') > 1);
+    });
+  }
+
+  // I cannot fathom why this doesn't exist in underscore.
+  function count(array, element){
+    return _.filter(array, function(elem){
+      return elem === element;
+    }).length;
   }
 
   function programmerDropped(event, ui){
     var teamName = $(this).parent().data('name');
     var programmer = ui.draggable;
     programmer.data('team-name', teamName);
+    checkTeams();
   }
 
   function setProgrammerTeam(){
@@ -14,7 +34,13 @@ $(document).ready(function(){
     $(this).data('team-name', teamName);
   }
 
+  function toggleLead(){
+    $(this).toggleClass('lead');
+    checkTeams();
+  }
+
   $('.programmer').draggable().each(setProgrammerTeam);
   $('.programmer:not(.in-training)').dblclick(toggleLead);
   $('.team .container').droppable({ drop: programmerDropped });
+  checkTeams();
 });
